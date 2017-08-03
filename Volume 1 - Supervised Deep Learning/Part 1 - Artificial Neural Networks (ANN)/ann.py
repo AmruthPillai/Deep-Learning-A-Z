249cfab74c15b6bc19ac9690fbd8d5764982b219
Amruth Pillai <im.amruth@gmail.com>
2017-08-03 12:52:31 +0530

    completed upto step 10, training complete

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/ann.py
@@ -46,7 +46,34 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Predicting the Test set results
+###
+#Part 2 - Now, let's make an ANN!
+###
+
+# Importing the Keras libraries and required packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN, applying Stochastic Gradient Descent
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Predicting the test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
